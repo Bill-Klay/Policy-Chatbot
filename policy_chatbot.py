ea69e6bf7b1986f250e91fc7529cfa6aeaa36a62
Bilal Khan <bilal.khan@qordata.com>
2024-01-15 17:43:42 +0500

    update server

--- policy_chatbot.py
+++ policy_chatbot.py
@@ -101,5 +101,11 @@ def index():
     return "Hello Friend! Yes, the server is running 🏃"
 
 if __name__ == "__main__":
-    print("Application running on port 6970...")
-    serve(app, host='localhost', port=6970)
+    HOST = os.environ.get('SERVER_HOST', '10.0.100.176')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '6969'))
+    except ValueError:
+        PORT = 5555
+
+    print("Server running at ", HOST, " @ ", PORT)
+    serve(app.app, host=HOST, port=PORT)
